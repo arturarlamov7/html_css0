3a4a69e6bc07ddfa6c3c1391571a402a42509f14
Vladislav <vladislav.koselev@ivkhk.ee>
2020-09-14 09:59:56 +0300

    2lvl

--- Canvas_game/game.js
+++ Canvas_game/game.js
@@ -1,13 +1,58 @@
 let game = {
-	start: function() {
+	ctx: null, 
+	sprites: {
+		background: null, 
+		ball: null, 
+		platform: null
+	},
+	init: function() {
 		this.ctx = document.getElementById("mycanvas").getContext("2d");
-		let background = new Image();
-		background.src = "img/background.png";
+	},
+	preload(callback) {
+		let loaded = 0;
+		let required = Object.keys(this.sprites).length;
+
+		for (let key in this.sprites) {
+			this.sprites[key] = new Image();
+			this.sprites[key].src = "img/" + key + ".png";
+			this.sprites[key].addEventListener("load", () => {
+				++loaded;
+				if (loaded >= required) {
+					callback();
+				}	
+			});
+		}
+	},
+	run() {
 		window.requestAnimationFrame(() => {
-			this.ctx.drawImage(background, 0, 0);
+			this.render();
+		});
+	},
+	render() {
+		this.ctx.drawImage(this.sprites.background, 0, 0);
+		this.ctx.drawImage(this.sprites.ball, 0, 0);
+		this.ctx.drawImage(this.sprites.platform, 0, 0);
+	},
+	start: function() {
+		this.init();
+		this.preload(() => {
+			this.run();
 		});
 	}
+};		 
+
+game.ball = {
+	x: 320,
+	y: 280,
+	width: 20,
+	height: 20
 };
+
+game.platform = {
+	x: 280,
+	y: 300
+};
+
 window.addEventListener("load", () => {
-	game.start();
-});
+	game.star();
+});	
