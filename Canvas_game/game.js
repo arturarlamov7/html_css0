a43b07752f187cd3681e6a47990c64c3ce86249d
Vladislav <vladislav.koselev@ivkhk.ee>
2020-09-14 10:39:59 +0300

    lvl3

--- Canvas_game/game.js
+++ Canvas_game/game.js
@@ -1,9 +1,15 @@
 let game = {
 	ctx: null, 
+	platform: null,
+	ball: null,
+	block: [],
+	rows: 4,
+	cols: 8, 
 	sprites: {
-		background: null, 
-		ball: null, 
-		platform: null
+		background: null,
+		ball: null,
+		platform: null, 
+		block: null,
 	},
 	init: function() {
 		this.ctx = document.getElementById("mycanvas").getContext("2d");
@@ -11,18 +17,30 @@ let game = {
 	preload(callback) {
 		let loaded = 0;
 		let required = Object.keys(this.sprites).length;
+		let onImageLoad = () => {
+			++loaded;
+			if (loaded >= required) {
+				callback();
+			}
+		};
 
 		for (let key in this.sprites) {
 			this.sprites[key] = new Image();
 			this.sprites[key].src = "img/" + key + ".png";
-			this.sprites[key].addEventListener("load", () => {
-				++loaded;
-				if (loaded >= required) {
-					callback();
-				}	
-			});
+			this.sprites[key].addEventListener("load", onImageLoad);
 		}
 	},
+	create() {
+		for (let row = 0; row < this.rows; row++) {
+			for (let col = 0; col < this.cols; col++) {
+				this.block.push({
+					x: 64 * col + 65,
+					y: 24 * col + 35
+				});
+			}
+		}
+	},
+
 	run() {
 		window.requestAnimationFrame(() => {
 			this.render();
@@ -30,29 +48,37 @@ let game = {
 	},
 	render() {
 		this.ctx.drawImage(this.sprites.background, 0, 0);
-		this.ctx.drawImage(this.sprites.ball, 0, 0);
-		this.ctx.drawImage(this.sprites.platform, 0, 0);
+		this.ctx.drawImage(this.sprites.ball, 0, 0, this.ball.width, this.ball.height,
+			this.ball.x, this.ball.y, this.ball.width, this.ball.height);
+		this.ctx.drawImage(this.sprites.platform, this.platform.x, this.platform.y);
+		this.rederBlocks();
+	},
+	renderBlocks() {
+		for (let block of this.blocks) {
+			this.ctx.drawImage(this.sprites.blocks, block.x, block.y);
+		}
 	},
 	start: function() {
 		this.init();
 		this.preload(() => {
+			this.create();
 			this.run();
 		});
 	}
-};		 
+};
 
 game.ball = {
 	x: 320,
-	y: 280,
-	width: 20,
+	y: 280, 
+	width: 20, 
 	height: 20
 };
 
 game.platform = {
-	x: 280,
+	x: 280, 
 	y: 300
 };
 
 window.addEventListener("load", () => {
 	game.start();
-});	
+});
